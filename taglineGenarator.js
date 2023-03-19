ca6316566bfaeb666cd8ca60b45611e2ec2f0e79
mominul-hoque <ulhouqemomin39@gmail.com>
2023-03-19 09:26:12 +0600

    updating

--- taglineGenarator.js
+++ taglineGenarator.js
@@ -15,33 +15,34 @@ function generateTag(arr, interval) {
   setInterval(() => {
     const randomIndex = Math.floor(Math.random() * arr.length);
     const randomTagLine = tagLines[randomIndex];
-    document.getElementById("taglines").innerText = randomTagLine;
+    document.getElementById("tagline").innerText = randomTagLine;
   }, interval);
 }
 
-generateTag(tagLines, 2000);
+generateTag(tagLines, 3000);
 
 document
   .getElementById("random-room-btn")
   .addEventListener("click", async function () {
     console.log("hello world");
+
     const data = await fetch(`ROOMS.json`);
     const result = await data.json();
     const randomIndex = Math.floor(Math.random() * 10);
     const modalBody = document.getElementById("random-room-info-modal-body");
-    const { name, summary, property_type, images, review_scores } = result[9];
-    
+    const { name, summary, property_type, images, review_scores } = result[randomIndex];
     
 
     const reviews = document.createElement("ol");
     if (review_scores.scores) {
       reviews.setAttribute('class',"list-group")
 
-      object.keys(review_scores.scores).forEach((key) => {
+      Object.keys(review_scores.scores).forEach((key) => {
+
         reviews.innerHTML += `
         <li class="list-group-item list-group-item-info d-flex justify-content-between align-items-center">
         ${key} : 
-        <span class="badge bg-primary rounded-pill">${review_scores.scores[key]}</span> 
+        <span class="badge bg-primary rounded-pill">${review_scores?.scores[key]}</span> 
         </li>
         `;
       });
@@ -61,7 +62,8 @@ document
         <p class="card-text">${summary}</p>
         <div id='review-score'>
       Review Scores :
-      ${review_scores.scores.review_scores_accuracy}
+      <div id="reviews">
+      </div>
     </div>
       </div>
       <button class="btn btn-info btn-lg"  role="button"
@@ -69,6 +71,8 @@ document
     </div>
    </div>
     `;
+    document.getElementById('reviews').appendChild(reviews);
+ 
     
   });
 
